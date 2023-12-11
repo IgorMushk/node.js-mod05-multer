c79b9840bbaeaf15398cddf7d26d11af0c0be862
IgorMushk <igor.mushketik@gmail.com>
2023-12-11 14:02:59 +0200

    created multer middleware _

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 const express = require("express");
 const cors = require("cors");
+const multer = require("multer");
+const path = require("path");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+const tempDir = path.join(__dirname, "temp");
+
+const multerConfig = multer.diskStorage({
+    destination: tempDir,
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    }
+});
+
+const upload = multer({
+    storage: multerConfig
+})
 
 const books = [];
 
